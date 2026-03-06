5af868ca7f403e1719f5f75c4255deaef9555a46
diogosalvadorb <diogosalvadorb@gmail.com>
2026-03-05 21:11:27 -0300

    feat: config hangfire and job mark overdue loan

---
+++ LibraryManagement.Application/Jobs/OverdueLoanJob.cs
@@ -0,0 +1,34 @@
+﻿using LibraryManagement.Domain.Enums;
+using LibraryManagement.Domain.Interfaces;
+
+namespace LibraryManagement.Application.Jobs
+{
+    public class OverdueLoanJob
+    {
+        private readonly ILoanRepository _loanRepository;
+
+        public OverdueLoanJob(ILoanRepository loanRepository)
+        {
+            _loanRepository = loanRepository;
+        }
+
+        public async Task CheckAndMarkOverdueLoansAsync()
+        {
+            var loans = await _loanRepository.GetAllAsync();
+
+            Console.WriteLine($"Checking for overdue loans at {DateTime.UtcNow}...");
+
+            var overdueLoans = loans.Where(l =>
+                l.Active &&
+                l.LoanStatus == LoanStatus.Borrowed &&
+                l.ExpectedReturnDate < DateTime.UtcNow)
+                .ToList();
+
+            foreach (var loan in overdueLoans)
+            {
+                loan.MarkAsOverdue();
+                await _loanRepository.UpdateAsync(loan);
+            }
+        }
+    }
+}
